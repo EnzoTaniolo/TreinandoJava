b4d9ca666766ad166f90aeec28e8deba157f692d
enzot <enzotaniolofort@gmail.com>
2024-08-15 11:19:25 -0300

    teste

---
+++ src/PersonagemMagico.java
@@ -0,0 +1,17 @@
+public class PersonagemMagico {
+
+    String nome;
+
+    int nivelEnergia;
+
+    String poderMagico;
+
+    public PersonagemMagico(String nome, int nivelEnergia, String poderMagico){
+        this.nome = nome;
+        this.nivelEnergia = nivelEnergia;
+        this.poderMagico = poderMagico;
+    }
+    public PersonagemMagico(String nome) { this.nome = nome; }
+
+    public PersonagemMagico(){}
+}
